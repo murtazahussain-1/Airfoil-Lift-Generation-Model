7c7f2669001e43648e75dfd4280ffb5174ef3bbf
murtazahussain-1 <murtaza.hussain018@gmail.com>
2023-05-22 15:05:30 +0500

    Fixing compilation error

---
+++ src/Bernoulli/README.md
@@ -0,0 +1,1 @@
+# Model using Bernoulli's Principle
--- src/config.mk
+++ src/config.mk
@@ -1,7 +1,7 @@
 # Lines starting with '#' are comments.
 
 # Explicitely specify which files to compile
-SOURCES = link.c
+SOURCES		= link.c
 
 # Options that get passed to the C compiler
 #CFLAGS = -DDEBUG
